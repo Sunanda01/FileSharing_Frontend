9f54ede9003b4e48c112e933d05b024a67ee71d2
Sunanda01 <sunandasadhukhan1@gmail.com>
2024-11-25 18:25:32 +0530

    fix headers in axios

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
-const Api_URL='http://localhost:8000';
+const Api_URL=import.meta.env.VITE_API_URL;
 export const uploadFile=async(data)=>{
     try{
-        let res=await axios.post(`${Api_URL}/upload`,data); 
+        let res=await axios.post(`${Api_URL}/upload`,data,{
+            headers: {
+              "Content-Type": "multipart/form-data"
+            }
+          }); 
         return res.data;
     }catch(err){
         console.error(err.message);
